e822470f86e58e0a0da8a2d0f34dc1aa93fcee44
Maxence <coddyumpro@gmail.com>
2024-05-24 10:16:39 +0200

    Update
    Changement des images et de format, ajout du bg dans le css de app.css et suppression de la card actuelle qui ne plais pas niveau style

--- src/components/HackerCard.jsx
+++ src/components/HackerCard.jsx
@@ -1,31 +1,8 @@
 import React from "react";
 import hackerData from "./Hacker.json";
 
-function HackerCard({ pseudo, name, image, bio }) {
-    return (
-        <div className="w-72 h-96 m-12 bg-gray-900 rounded-3xl">
-            <div className="w-80 h-auto">
-                {/* todo : changer le src de l'image une fois que 
-                j'aurais compris comment sync avec le JSON */}
-                <img
-                    className="w-72 rounded-t-3xl"
-                    src="src/assets/img/Hector-Xavier-Monsegur.jpeg"
-                    alt=""
-                />
-            </div>
-            <h2 className="text-white pl-3 pt-2 font-bold">
-                Pseudo : <span className="text-white">{hackerData.pseudo}</span>
-            </h2>
-            <h2 className="text-white pl-3 pt-2 font-bold">
-                Name : <span className="text-white">{hackerData.name}</span>
-            </h2>
-
-            <p className="text-white pl-3 pt-2 font-bold">
-                Bio : {hackerData.bio}
-            </p>
-        </div>
-    );
+function HackerCard() {
+    return
 }
 
-/* Todo : set je Json  */
 export default HackerCard;
